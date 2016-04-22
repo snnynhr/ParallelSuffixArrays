d577e440d709a657f8efc94018546f11a7f5d9bb
Sunny Nahar <sunnyn@cmu.edu>
2016-04-22 18:53:04 -0400

    added components

--- src/main.cpp
+++ src/main.cpp
@@ -42,9 +42,60 @@ int main(int argc, char* argv[]) {
   }
   fprintf(stdout, "%d: %.*s", myid, static_cast<uint32_t>(size), data);
 
-  //MPI_Barrier(MPI_COMM_WORLD);
+  /*
+   *  Begin construction!
+   */
 
-  // Begin construction
+  /*
+   *  @TODO: Component 1:
+   *  S = <(T[i,i+2], i) : i \in [0,n), i mod 3 \not= 0>
+   */
+
+  /*
+   *  @TODO: Component 2:
+   *  sort S by first component
+   */
+
+  /*
+   *  @TODO: Component 3:
+   *  P := name (S)
+   */
+
+  /*
+   *  @TODO: Component 4:
+   *  If names are not unique:
+   *  Permute (r,i) in P such that they are sorted by (i mod 3, i div 3)
+   *  SA^{12} = pDC3(<c : (c,i) in P>)
+   *  P := <(j+1, mapBack(SA^{12}[j], n/3)) : j < 2n/3>
+   */
+
+  /*
+   *  @TODO: Component 5:
+   *  S_0 := <(T[i], T[i+1], c', c'', i) : i mod 3 = 0), (c',i+1), (c'', i+2) in
+   * P>
+   */
+
+  /*
+   *  @TODO: Component 6:
+   *  S_1 := <(c, T[i], c', i) : i mod 3 = 1), (c,i), (c', i+1) in P>
+   */
+
+  /*
+   *  @TODO: Component 7:
+   *  S_2 := <(c, T[i], T[i+1], c'', i) : i mod 3 = 2), (c,i), (c'', i+2) in P>
+   */
+
+  /*
+   *  @TODO: Component 8:
+   *  Sort S_0 union S_1 union S_2 using compare operator in paper.
+   */
+
+  /*
+   *  @TODO: Component 9:
+   *  Return last component of (s : s in S).
+   */
+
+  // MPI_Barrier(MPI_COMM_WORLD);
 
   // Done
   free(data);
